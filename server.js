3d4d3bf4d9bd7ce9a6b644bfaeefb508a0148426
borisovaas <borisovaas@oooinex.ru>
2023-01-10 21:12:46 +0300

    Sprint 1

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+
+app.use(express.static(`${__dirname}/dist`));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(`${__dirname}/dist/index.html`))
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`)
+})
